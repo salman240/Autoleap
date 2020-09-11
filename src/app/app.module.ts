563d6a9cf549375dca6358a2d15174c4e78b50df
salman240 <salmanyousaf240@gmail.com>
2020-09-11 17:58:46 +0500

    generated component for different screens

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TodoContainerComponent } from './components/todo-container/todo-container.component';
+import { TodoComponent } from './components/todo/todo.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoContainerComponent,
+    TodoComponent,
+    NavbarComponent,
+    RegisterComponent,
+    LoginComponent,
+    ResetPasswordComponent,
   ],
   imports: [
     BrowserModule,
